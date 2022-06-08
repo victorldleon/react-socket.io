22d77cd3e2482083d8112bc9c761223c73c8d9a2
Victor De León <victorldleon@gmail.com>
2022-06-08 11:11:34 -0600

    implements socket.io in the server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,31 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+
+const { Server } = require('socket.io'); 
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET','POST'],
+        allowedHeaders: 'Content-Type, Authorization, X-Requested-With, X-Socket-ID',
+        credentials: true,
+        maxAge: '86400'
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('socket id', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('socket disconnected', socket.id);
+    });
+})
+
+server.listen(3001, () => {
+    console.log('Server running on port 3000');
+    }
+);

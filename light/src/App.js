2e8e67f63c55537087d878585d41c876e0b54c91
Victor De León <victorldleon@gmail.com>
2022-07-25 13:30:15 -0600

    adds new traffic light

--- light/src/App.js
+++ light/src/App.js
@@ -14,9 +14,8 @@ function App() {
   
   return (
     <div className="App">
-      <h1>Traffic Light</h1>
       <TrafficLight socket={socket}/>
-      <button onClick={handleEmit}>Turn Light to Green</button>
+      {/* <button onClick={handleEmit}>Turn Light to Green</button> */}
     </div>
   );
 }
